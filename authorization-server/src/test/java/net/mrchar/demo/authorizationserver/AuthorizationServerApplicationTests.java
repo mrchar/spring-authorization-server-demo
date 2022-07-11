53f712ecd36369d79305c2b054773acd0b7d97cf
mrchar <mrchar@qq.com>
2022-07-11 17:27:00 +0800

    添加authorization-server

---
+++ authorization-server/src/test/java/net/mrchar/demo/authorizationserver/AuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mrchar.demo.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
